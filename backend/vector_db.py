0e7ecdf925397771415efb07b645d291bee09355
Amarta113 <amartawaghani@gmail.com>
2026-03-13 18:44:44 +0500

    added chunks loaders embeddings functions

--- backend/vector_db.py
+++ backend/vector_db.py
@@ -1,6 +1,8 @@
 from langchain_community.document_loaders import PDFPlumberLoader
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import Chroma
+from chromadb.config import Settings
+from chromadb import Client
 
 pdfs_directory = "pdfs/"
 
@@ -8,13 +10,35 @@ def upload_pdf(file):
     with open(pdfs_directory + file.name, "wb") as f:
         f.write(file.getbuffer())
 
-
 def load_pdf(file_path):
     loader = PDFPlumberLoader(file_path)
     documents = loader.load()
     return documents
 
-def test_for_loader()
+def create_chunks(documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+    chunk = text_splitter.split_documents(documents)
+    return chunk
+
+def create_embeddings(chunks):
+    
+    pass
+
+def create_vectors(chunks, embeddings):
+    client = Client(Settings())
+    collection = client.create_collection(name="dataframe collection")
+    # here we are adding docs and embedding to chromadb
+    for idx, chunk in enumerate(chunks):
+        collection.add(
+            documents=[chunk.page_content],
+            metadatas=[{"id": idx}],
+            embeddings=[embeddings[idx]],
+            ids=[str(idx)] # here we are ensuring ids are string
+        )
+
 
 
 
